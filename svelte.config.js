c824355aa57aaee88d1e31ce49db6d0151690729
inexistentHeap404 <sanjayv0521@gmail.com>
2025-04-21 12:30:55 +0530

    svelte config changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
     // no need for prerender.entries or strict here
   }
 };
